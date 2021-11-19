9a1124896108d7e683e389e6a24c44b3567b8815
Petar Mihaylov <petar.m@studio350.com>
2021-11-19 13:22:13 -0500

    Initial implementation of console command

--- src/commands/console.ts
+++ src/commands/console.ts
@@ -1,10 +1,9 @@
 import {Command, flags} from '@oclif/command'
-import { biStreamingServiceErrors, Clients, executeReport, getReportParameters, logOff, RaasCredential, RaasRetrieveReportCallResult, retrieveReport, RetrieveReportResponse } from '..';
-import { config, login, RaasExecuteReportCallResult, RaasLogOffCallResult, RaasLogOnCallResult } from '../core-raas';
-import { biDataServiceErrors } from '../error';
-import { yellow, blue, magenta, green, red } from 'chalk';
-import cli from 'cli-ux'
+import { red } from 'chalk'
 import * as inquirer from 'inquirer'
+import { executeReportAction, getReportParametersAction, logOffAction, logOnAction, retrieveReportActon } from '../lib/actions'
+import { Clients, config, RaasCredential, RaasExecuteReportCallResult, RaasLogOnCallResult, RaasRetrieveReportCallResult } from '../lib/core-raas'
+import { decodeStream, FileExportExtensions, saveStream } from '../utils/formatters'
 
 export default class Pull extends Command {
   static description = 'Quickly test reports and access with this interactive console pulling data from a BI report through Reports as a Service (RAAS).'
@@ -17,6 +16,14 @@ export default class Pull extends Command {
   static flags = {
     // flag with no value (-h, --help)
     help: flags.help({char: 'h'}),
+    // flag with a value (-u, --username="VALUE")
+    username: flags.string({char: 'u', required: true, description: 'Username of user or service account. User is required for pulling UKG Time Management data.'}),
+    // flag with a value (-p, --password="VALUE")
+    password: flags.string({char: 'p', required: true, description: 'Password for the provided username.'}),
+    // flag with a value (-c, --customer-api-key="VALUE")
+    customerApiKey: flags.string({char: 'c', required: true, description: 'A 5-character alpha-numeric key from UKG Pro > MENU > SYSTEM CONFIGURATION > Security > Service Account Administration.'}),
+    // flag with a value (-a, --user-api-key="VALUE")
+    userApiKey: flags.string({char: 'a', required: true, description: 'A 12-character alpha-numeric key for the provided username from UKG Pro > MENU > SYSTEM CONFIGURATION > Security > Service Account Administration.'}),
     // flag with a value (-e, --base-endpoint-url="VALUE")
     baseEndpointUrl: flags.string(
       {char: 'e',
@@ -26,150 +33,247 @@ export default class Pull extends Command {
     // flag with no value (-v, --verbose)
     verbose: flags.boolean(
       {char: 'v', description: 'Output raw request/response combination for failing requests.'}
-    )
+    ),
+    console: flags.boolean(),
   }
 
   async run() {
     const {args, flags} = this.parse(Pull)
 
-    let baseEndpointUrl = flags.baseEndpointUrl;
-    let verbose = flags.verbose;
+    let responses: any = {};
 
-    let responses: any = await inquirer.prompt([{
-      name: 'baseEndpointUrl',
-      message: 'Select your environment:',
-      type: 'rawlist',
-      // Ask for this when the base endpoint was not set via a parameter
-      when: () => {
-        return flags.baseEndpointUrl === undefined;
+    if ( flags.baseEndpointUrl === undefined ||
+      flags.username === undefined ||
+      flags.password === undefined ||
+      flags.customerApiKey === undefined ||
+      flags.userApiKey === undefined ||
+      flags.verbose === undefined
+    ) {
+      responses  = await inquirer.prompt([{
+        name: 'baseEndpointUrl',
+        message: 'Select your environment:',
+        type: 'rawlist',
+        // Ask for this when the base endpoint was not set via a parameter
+        when: () => {
+          return flags.baseEndpointUrl === undefined;
+        },
+        default: 8,
+        loop: true,
+        choices: [
+          new inquirer.Separator('PRODUCTION Environments'),
+          {name: 'service2.ultipro.com'},
+          {name: 'service3.ultipro.ca'},
+          {name: 'service4.ultipro.com'},
+          {name: 'service5.ultipro.com'},
+          new inquirer.Separator('TEST Environments'),
+          {name: 'rental2.ultipro.com'},
+          {name: 'rental3.ultipro.ca'},
+          {name: 'rental4.ultipro.com'},
+          {name: 'rental5.ultipro.com'},
+          {name: 'servicet.ultipro.com'},
+        ]
       },
-      default: 8,
-      loop: true,
-      choices: [
-        new inquirer.Separator('PRODUCTION Environments'),
-        {name: 'service2.ultipro.com'},
-        {name: 'service3.ultipro.ca'},
-        {name: 'service4.ultipro.com'},
-        {name: 'service5.ultipro.com'},
-        new inquirer.Separator('TEST Environments'),
-        {name: 'rental2.ultipro.com'},
-        {name: 'rental3.ultipro.ca'},
-        {name: 'rental4.ultipro.com'},
-        {name: 'rental5.ultipro.com'},
-        {name: 'servicet.ultipro.com'},
-      ]
-    },
-    {
-      name: 'maskPassword',
-      message: 'Mask password input?',
-      type: 'confirm',
-      default: true,
-    },
-    {
-      name: 'username',
-      message: 'Enter an employee username or a service account username:',
-      type: 'input',
-      validate: (input) => input.length !== 0 ? true : 'Username cannot be blank. Please enter a username.'
-    },
-    {
-      name: 'password',
-      message: 'Enter the password:',
-      type: 'password',
-      when: (responses: any) => responses.maskPassword === true,
-      mask: '*',
-      validate: (input) => input.length !== 0 ? true : 'Password cannot be blank. Please enter a password.'
-    },
-    {
-      name: 'password',
-      message: 'Enter the password:',
-      type: 'input',
-      when: (responses: any) => responses.maskPassword === false,
-      validate: (input) => input.length !== 0 ? true : 'Password cannot be blank. Please enter a password.'
-    },
-    {
-      name: 'userApiKey',
-      message: 'Enter User API Key (12-characters):',
-      type: 'input',
-      validate: (input) => input.length === 12 ? true : 'Please enter a valid 12-character User API Key from UKG Pro > MENU > SYSTEM CONFIGURATION > Security > Service Account Administration (Service Accounts) | Web Services (Employee Users) page. Service Accounts that have the same Username in PROD and TEST (if available) may still have different User API Keys in each environment.'
-    },
-    {
-      name: 'customerApiKey',
-      message: 'Enter Customer API Key (5-characters):',
-      type: 'input',
-      validate: (input) => input.length === 5 ? true :  'Please enter a valid 5-character ampha-numeric Customer API Key. This is the 5-character alpha-numeric key listed at the top-left in UKG Pro > MENU > SYSTEM CONFIGURATION > Security > Service Account Administration page. The Customer API Key in PRODUCTION and TEST (if available) are different.'
-    },
-    {
-      name: 'verbose',
-      message: 'Verbose output?\n(Print the raw request/response pair for calls that result in an error.)',
-      type: 'confirm',
-      // Ask for this when the verbosity flag is not passed endpoint was not set via a parameter
-      when: () => {
-        return flags.verbose === undefined;
+      {
+        name: 'maskPassword',
+        message: 'Mask password input?',
+        type: 'confirm',
+        default: true,
+        when: () => {
+          return flags.password === undefined;
+        },
       },
-      default: false
-    },
-    {
-      name: 'verbose',
-      message: 'WARNING: Earlier you selected to hide the password. Error on the LogOn step will print your credentials to the console when Verbose output is selected. Please confirm Verbose output should be enabled:',
-      type: 'confirm',
-      askAnswered: true,
-      // Ask for this when the verbosity flag is not passed endpoint was not set via a parameter
-      when: (responses: any) => {
-        return responses.maskPassword === true && responses.verbose === true
+      {
+        name: 'username',
+        message: 'Enter an employee username or a service account username:',
+        type: 'input',
+        validate: (input) => input.length !== 0 ? true : 'Username cannot be blank. Please enter a username.',
+        when: () => {
+          return flags.username === undefined;
+        },
       },
-      default: false
+      {
+        name: 'password',
+        message: 'Enter the password:',
+        type: 'password',
+        when: (responses: any) => responses.maskPassword === true && flags.password === undefined,
+        mask: '*',
+        validate: (input) => input.length !== 0 ? true : 'Password cannot be blank. Please enter a password.'
+      },
+      {
+        name: 'password',
+        message: 'Enter the password:',
+        type: 'input',
+        when: (responses: any) => responses.maskPassword === false && flags.password === undefined,
+        validate: (input) => input.length !== 0 ? true : 'Password cannot be blank. Please enter a password.'
+      },
+      {
+        name: 'userApiKey',
+        message: 'Enter User API Key (12-characters):',
+        type: 'input',
+        validate: (input) => input.length === 12 ? true : 'Please enter a valid 12-character User API Key from UKG Pro > MENU > SYSTEM CONFIGURATION > Security > Service Account Administration (Service Accounts) | Web Services (Employee Users) page. Service Accounts that have the same Username in PROD and TEST (if available) may still have different User API Keys in each environment.',
+        when: () => {
+          return flags.userApiKey === undefined;
+        },
+      },
+      {
+        name: 'customerApiKey',
+        message: 'Enter Customer API Key (5-characters):',
+        type: 'input',
+        validate: (input) => input.length === 5 ? true :  'Please enter a valid 5-character ampha-numeric Customer API Key. This is the 5-character alpha-numeric key listed at the top-left in UKG Pro > MENU > SYSTEM CONFIGURATION > Security > Service Account Administration page. The Customer API Key in PRODUCTION and TEST (if available) are different.',
+        when: () => {
+          return flags.customerApiKey === undefined;
+        },
+      },
+      {
+        name: 'verbose',
+        message: 'Verbose output?\n(Print the raw request/response pair for calls that result in an error.)',
+        type: 'confirm',
+        // Ask for this when the verbosity flag is not passed endpoint was not set via a parameter
+        when: () => {
+          return flags.verbose === undefined;
+        },
+        default: false
+      },
+      {
+        name: 'verbose',
+        message: 'WARNING: Earlier you selected to hide the password. Error on the LogOn step will print your credentials to the console when Verbose output is selected. Please confirm Verbose output should be enabled:',
+        type: 'confirm',
+        askAnswered: true,
+        // Ask for this when the verbosity flag is not passed endpoint was not set via a parameter
+        when: (responses: any) => {
+          return responses.maskPassword === true && responses.verbose === true
+        },
+        default: false
+      }
+      ]);
+      // Assign the answer for verbosity back to flags so it can be passed to the *Action methods
+      if (responses?.verbose) flags.verbose = responses.verbose;
     }
-    ]);
 
+    flags.console = true; // Signal to actions that the call is coming from the console command
+
+    const raasCredential: RaasCredential = {
+      UserName: responses.username || flags.username,
+      Password: responses.password || flags.password,
+      ClientAccessKey: responses.customerApiKey || flags.customerApiKey,
+      UserAccessKey: responses.userApiKey || flags.userApiKey
+    }
+
+    // Ask the user what they would like to do
     const beginPrompt = [{
       name: 'action',
       message: 'What would you like to do?',
       type: 'rawlist',
       // Ask for this when the base endpoint was not set via a parameter
-      when: () => {
-        return flags.baseEndpointUrl === undefined;
-      },
       default: 8,
       loop: true,
       choices: [
         {name: 'GetReportList'},
         {name: 'GetReportParameters'},
         {name: 'ExecuteReport'},
-        {name: 'exit'},
+        {name: 'RetrieveReport'},
+        {name: 'LogOff and Exit'},
       ]
     }]
 
     const getReportPathOrId = [{
       name: 'reportPathOrId',
       message: 'What is the Report Path or ID?',
-      type: 'input'
+      type: 'input',
+      filter: (input: any) => {
+        return input[0] === 'i' ? `storeID("${input}")` : input
+      }
     }]
+
+    const clients: Clients = await config(responses.baseEndpointUrl || flags.baseEndpointUrl);
+    const logOnResult: RaasLogOnCallResult = await logOnAction(clients, raasCredential, flags);
+
+    let reportPathOrId = ''
+    let reportKey: string | undefined = ''
+    // Used as a check to make sure the user retrieves reports for which they have started execution
+    let retrievedLastReport = true;
+    let executeReportResult: RaasExecuteReportCallResult;
 
     async function repl() {
       let answer = await inquirer.prompt(beginPrompt);
-      let reportPathOrId = ''
+
       let keepLooping = true;
+
+      const keepReportId = [{
+        name: 'keepReportId',
+        message: 'Enter a new Report Path or ID?',
+        type: 'confirm',
+        default: false,
+      }]
+
       switch (answer.action) {
         case 'GetReportList':
           console.log(`Getting Report List for: ${answer.reportPathOrId} `)
           break;
         case 'GetReportParameters':
-          answer = await inquirer.prompt(getReportPathOrId);
-          reportPathOrId = answer.reportPathOrId;
-          console.log(`Getting Parameters for: ${answer.reportPathOrId} `)
+          if (reportPathOrId === '') {
+            answer = await inquirer.prompt(getReportPathOrId);
+            reportPathOrId = answer.reportPathOrId;
+          } else {
+            answer = await inquirer.prompt(keepReportId);
+            if (answer.keepReportId) {
+              answer = await inquirer.prompt(getReportPathOrId);
+              reportPathOrId = answer.reportPathOrId;
+            }
+          }
+
+          await getReportParametersAction(clients, logOnResult, reportPathOrId, flags)
+          .catch ((e: any) => {
+            console.error(e.message);
+          })
+
           break;
         case 'ExecuteReport':
-          answer = await inquirer.prompt(getReportPathOrId);
-          reportPathOrId = answer.reportPathOrId;
-          console.log(`Executing Report for: ${answer.reportPathOrId} `)
+          if (retrievedLastReport) {
+            if (reportPathOrId === '') {
+              answer = await inquirer.prompt(getReportPathOrId);
+              reportPathOrId = answer.reportPathOrId;
+            } else {
+              answer = await inquirer.prompt(keepReportId);
+              if (answer.keepReportId) {
+                answer = await inquirer.prompt(getReportPathOrId);
+                reportPathOrId = answer.reportPathOrId;
+              }
+            }
+            try {
+              executeReportResult = await executeReportAction(clients, logOnResult, reportPathOrId, flags)
+            } catch (e: any) {
+              console.error(e.message);
+            }
+
+            // If there are  no errors store the report key and indicate there is a pending report
+            if (!executeReportResult?.hasErrors) {
+              reportKey = executeReportResult?.result[0].ExecuteReportResult.ReportKey
+              retrievedLastReport = false;
+            }
+          } else {
+            console.log(red('Running more than one (1) report is not supported. Please run RetrieveReport first.'));
+          }
           break;
-        case 'exit':
+        case 'RetrieveReport':
+          if (reportKey !== '' && retrievedLastReport === false) {
+            const retrieveReportResult: RaasRetrieveReportCallResult = await retrieveReportActon(clients, executeReportResult, flags);
+            const decodedStream = await decodeStream(retrieveReportResult.result[0].ReportStream);
+            await saveStream(decodedStream, 'export', FileExportExtensions.XML);
+            reportKey = ''
+            retrievedLastReport = true
+          } else (
+            console.log(red('You must ExecuteReport before you can RetrieveReport.'))
+          )
+          break;
+        case 'LogOff and Exit':
           keepLooping = false
           break;
       }
       if (keepLooping) repl()
     }
 
-    repl();
+    await repl();
+    await logOffAction(clients, logOnResult, flags);
   }
 }
