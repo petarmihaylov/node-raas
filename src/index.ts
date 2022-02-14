8450da691df2951bf092b4f655cd77f5b3d0e37a
Petar Mihaylov <petar.m@studio350.com>
2022-02-14 11:56:42 -0500

    Export utils for easier decode/save of stream

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 // export {run} from '@oclif/command'
-export * from './lib/core-raas'
-export * from './lib/actions'
-export * from './lib/errors'
+export * from './lib/core-raas';
+export * from './lib/actions';
+export * from './lib/errors';
+export * from './utils/formatters';
